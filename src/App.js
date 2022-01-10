f5363947ecbe7311d403a266b012563dc121b263
sarahc987 <89328572+sarahc987@users.noreply.github.com>
2022-01-11 08:11:02 +1000

    Created basic search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <header>
+          <h1>Dictionary App</h1>
+        </header>
+        <Dictionary />
+        <footer className="App-footer">Coded by Sarah Cox</footer>
+      </div>
+    </div>
+  );
 }
