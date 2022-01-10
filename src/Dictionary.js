dd4d2ac98b62ea8aa4f257b9adb572a2c1d9262b
sarahc987 <89328572+sarahc987@users.noreply.github.com>
2022-01-11 09:15:34 +1000

    Rendered meanings of searched words

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -27,6 +29,7 @@ export default function Dictionary() {
         <input type="search" onChange={handleKeywordChange} autoFocus={true} />
         <input type="submit" />
       </form>
+      <Results results={results} />
     </div>
   );
 }
