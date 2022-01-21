387f039c2a094c620ccbea0a29fea27c914e9904
sarahc987 <89328572+sarahc987@users.noreply.github.com>
2022-01-21 11:02:37 +1000

    Finishing touches for CSS

--- src/App.js
+++ src/App.js
@@ -9,7 +9,15 @@ export default function App() {
           <h1>Dictionary</h1>
         </header>
         <Dictionary defaultKeyword="sunset" />
-        <footer className="App-footer">Coded by Sarah Cox</footer>
+        <footer className="App-footer">
+          Coded by{" "}
+          <a
+            href="https://github.com/sarahc987/dictionary-project"
+            alt="Github Repository for Dictionary App"
+          >
+            Sarah Cox
+          </a>
+        </footer>
       </div>
     </div>
   );
